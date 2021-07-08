6dc9ce42aa98a70f633d453e6060ef32e1b3c04a
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-08 08:39:31 -0700

    Part 2

--- ex_a/variables/src/main.rs
+++ ex_a/variables/src/main.rs
@@ -1,6 +1,13 @@
+const STARTING_MISSILES : i32 = 8;
+const READY_AMOUNT : i32 = 6;
+
 fn main() {
-    let missiles = 8;
-    let ready = 2;
+    let mut missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles -= ready;
+
+    println!("I now have {} missiles left...", missiles);
 }
