44e6c510c5aa234935b2752ac01b50ddb689818a
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-07 21:09:23 -0700

    Initialize Exercise A directory

---
+++ ex_a/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/kchew/projects/git/rust_practice/ex_a/variables/target/debug/deps/variables: src/main.rs
+
+/Users/kchew/projects/git/rust_practice/ex_a/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ ex_a/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/kchew/projects/git/rust_practice/ex_a/variables/target/debug/variables: /Users/kchew/projects/git/rust_practice/ex_a/variables/src/main.rs
