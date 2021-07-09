90149e77f32435a02c925842f4fffc2ba9859953
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-08 19:33:14 -0700

    Exercise A Parts 1/2

---
+++ ex_a/variables/target/debug/deps/variables-1cf47689f096a833.d
@@ -0,0 +1,5 @@
+/Users/kchew/projects/git/rust_practice/ex_a/variables/target/debug/deps/variables-1cf47689f096a833.rmeta: src/main.rs
+
+/Users/kchew/projects/git/rust_practice/ex_a/variables/target/debug/deps/variables-1cf47689f096a833.d: src/main.rs
+
+src/main.rs:
