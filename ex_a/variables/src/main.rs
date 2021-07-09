13488ca46e516f0ae4ca3ff1ec9d3df6a84ff4f9
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-08 19:39:42 -0700

    Exercise A: remove uncessary comments

--- ex_a/variables/src/main.rs
+++ ex_a/variables/src/main.rs
@@ -2,8 +2,6 @@ const STARTING_MISSILES : i32 = 8;
 const READY_AMOUNT : i32 = 6;
 
 fn main() {
-    //let mut missiles : i32 = STARTING_MISSILES;
-    //let ready : i32 = READY_AMOUNT;
     let (mut missiles, ready) : (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
 
     println!("Firing {} of my {} missiles...", ready, missiles);
