103b98bf36ef5fe6dd70ec951ef9e0656388bafe
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-09 07:33:44 -0700

    Exercise b: Finished exercise

--- ex_b/functions/src/main.rs
+++ ex_b/functions/src/main.rs
@@ -1,3 +1,24 @@
+// silence warnings for practice
+#![allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+    
+    {
+        let area = area_of(width, height);
+        println!("Area is {}", area);
+    }
+    
+    let volume = volume_of(width, height, depth);
+    println!("Volume is {}", volume);
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn volume_of(x: i32, y: i32, z: i32) -> i32 {
+    x * y * z
 }
