863ee8376a7d6f8beb36af696744628dfaaf576d
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-08 19:44:37 -0700

    Exercise A: Finished challenge exercises

--- ex_a/variables/src/main.rs
+++ ex_a/variables/src/main.rs
@@ -2,12 +2,9 @@ const STARTING_MISSILES : i32 = 8;
 const READY_AMOUNT : i32 = 6;
 
 fn main() {
-    let (mut missiles, ready) : (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
-    let test : String = "Test".to_string();
+    let (missiles, ready) : (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
 
     println!("Firing {} of my {} missiles...", ready, missiles);
-
-    //missiles -= ready;
 
     println!("I now have {} missiles left...", missiles - ready);
 }
