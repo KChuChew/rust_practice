d54e49f4312ed1ed2c7f6acb4f91bcf095ce1537
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-09 18:16:49 -0700

    test

--- ex_b/functions/target/debug/functions.d
+++ ex_b/functions/target/debug/functions.d
@@ -1,1 +1,1 @@
-/Users/kchew/projects/git/rust_practice/ex_b/functions/target/debug/functions: /Users/kchew/projects/git/rust_practice/ex_b/functions/src/main.rs
+/Users/kchew/projects/git/rust_practice/ex_b/functions/target/debug/functions: /Users/kchew/projects/git/rust_practice/ex_b/functions/src/lib.rs /Users/kchew/projects/git/rust_practice/ex_b/functions/src/main.rs
