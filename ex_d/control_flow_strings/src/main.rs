267826f74933540a737a3ff539806542ec0af270
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-12 19:10:30 -0700

    Exercise D: Finish exercise

--- ex_d/control_flow_strings/src/main.rs
+++ ex_d/control_flow_strings/src/main.rs
@@ -8,18 +8,53 @@ fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
 
     for arg in args {
-        println!("{}", arg);
+        if arg == "sum" {
+            sum(); 
+            continue;
+        }
+
+        if arg == "double" {
+            double();
+            continue;
+        }
+
+        count(arg);
     }
 }
 
 fn sum() {
+    let mut sum = 0; 
+    for val in 7..=23 {
+        sum += val;
+    }
 
+    println!("Sum of vals 7 -> 23 inclusive {}", sum);
+    println!();
 }
 
 fn double() {
+    let mut count = 0;
+    let mut x = 1;
 
+    while x <= 500 {
+        x *= 2;
+        count += 1;
+    }
+
+    println!("Double starting value 1 {} times to exceed val of 500", count);
 }
 
 fn count(arg: String) {
+    let mut loop_count = 0;
 
+    loop {
+        if loop_count == 8 {
+            break;
+        }
+
+        print!("{} ", arg);
+        loop_count += 1; 
+    }
+
+    println!();
 }
