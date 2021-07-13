faf316eafe4b1fcb0dd4df5b7e9125c9e01ca145
Kevin Chu <kevin.git.chu@gmail.com>
2021-07-12 18:53:29 -0700

    Exercise D: added notes for cmd line reading

--- ex_d/control_flow_strings/src/main.rs
+++ ex_d/control_flow_strings/src/main.rs
@@ -1,3 +1,25 @@
+#![allow(dead_code, unused_mut, unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+
+    // std::env::args() -> returns arguments program started w/ via cmd line
+    // skip(1) -> skips first argument run?
+    // collect -> creates collection from arguments this case is vector
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    for arg in args {
+        println!("{}", arg);
+    }
+}
+
+fn sum() {
+
+}
+
+fn double() {
+
+}
+
+fn count(arg: String) {
+
 }
